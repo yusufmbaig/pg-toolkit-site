6fc50029a94bc9f641a5dd242c5e25e2deefba77
your.user.name <yusufwithu@gmail.com>
2024-05-11 23:49:36 -0500

    Feature: Footer Outlay Done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 function Home() {
   return (
     <>
       <Navbar />
-      <div>
+      <div className="HomeContent">
         <h1>Home</h1>
       </div>
+      <Footer />
     </>
   );
 }
